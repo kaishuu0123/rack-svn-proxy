f4855b75b143cf2ee1588bd65901ebc0e18afc14
Koki Oyatsu <kaishuu0123@gmail.com>
2017-07-23 23:03:17 +0900

    first commit (by bundle gem . -t)

---
+++ lib/rack/svn/proxy.rb
@@ -0,0 +1,9 @@
+require "rack/svn/proxy/version"
+
+module Rack
+  module Svn
+    module Proxy
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack/svn/proxy/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Svn
+    module Proxy
+      VERSION = "0.1.0"
+    end
+  end
+end
